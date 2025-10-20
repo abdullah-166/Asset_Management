948b7fd38ef824f118293aecb276fa5ce3b9a99d
Abdullah Al Arif <Abdullah Al Arif@DESKTOP-AL7QNR3>
2025-10-20 13:12:42 +0600

    Add project files.

---
+++ Asset_Management/Controllers/AssetsController.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Asset_Management.Data;
+using Asset_Management.Models;
+
+namespace Asset_Management.Controllers
+{
+    public class AssetsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AssetsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Assets
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Asset.ToListAsync());
+        }
+
+        // GET: Assets/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var asset = await _context.Asset
+                .FirstOrDefaultAsync(m => m.AssetId == id);
+            if (asset == null)
+            {
+                return NotFound();
+            }
+
+            return View(asset);
+        }
+
+        // GET: Assets/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Assets/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("AssetId,AssetTag,Name,Category,Brand,Model,SerialNumber,PurchaseDate,PurchaseOrderNo,Supplier,PurchasePrice,WarrantyEndDate,Location,Status,AssignedToUserId,AssignedAt,QRCodeValue,QRCodeGeneratedAt,QRCodePrinted,Notes,IsActive")] Asset asset)
+        {
+            if (ModelState.IsValid)
+            {
+                asset.AssetId = Guid.NewGuid();
+                _context.Add(asset);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(asset);
+        }
+
+        // GET: Assets/Edit/5
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var asset = await _context.Asset.FindAsync(id);
+            if (asset == null)
+            {
+                return NotFound();
+            }
+            return View(asset);
+        }
+
+        // POST: Assets/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("AssetId,AssetTag,Name,Category,Brand,Model,SerialNumber,PurchaseDate,PurchaseOrderNo,Supplier,PurchasePrice,WarrantyEndDate,Location,Status,AssignedToUserId,AssignedAt,QRCodeValue,QRCodeGeneratedAt,QRCodePrinted,Notes,IsActive")] Asset asset)
+        {
+            if (id != asset.AssetId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(asset);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AssetExists(asset.AssetId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(asset);
+        }
+
+        // GET: Assets/Delete/5
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var asset = await _context.Asset
+                .FirstOrDefaultAsync(m => m.AssetId == id);
+            if (asset == null)
+            {
+                return NotFound();
+            }
+
+            return View(asset);
+        }
+
+        // POST: Assets/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            var asset = await _context.Asset.FindAsync(id);
+            if (asset != null)
+            {
+                _context.Asset.Remove(asset);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AssetExists(Guid id)
+        {
+            return _context.Asset.Any(e => e.AssetId == id);
+        }
+    }
+}
---
+++ Asset_Management/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Asset_Management.Models;
+
+namespace Asset_Management.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Asset_Management.Models.Asset> Asset { get; set; } = default!;
+    }
+}
